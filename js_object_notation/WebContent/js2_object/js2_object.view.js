fdaf45a88c793e36aa8edbacb0be618b341910cb
biyvaqa12 <halilural5@gmail.com>
2017-05-10 14:20:58 +0300

    changes

---
+++ js_object_notation/WebContent/js2_object/js2_object.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("js2_object.js2_object", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf js2_object.js2_object
+	*/ 
+	getControllerName : function() {
+		return "js2_object.js2_object";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf js2_object.js2_object
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
